72a6c002767693b4c7888261d80d18882749edb4
jianghe <573748150@163.com>
2018-01-26 19:00:53 +0800

    数据驱动界面代码（单向数据流）

--- app/libs/linux/linux下mysql安装配置.md
+++ app/libs/linux/linux下mysql安装配置.md
@@ -4,3 +4,41 @@ https://devysql.com/downloads/repo/yum/ 查看linux下yum安装信息mysql57-com
 MYSQL 密码 2053440Zx,
 
 http://www.linuxidc.com/Linux/2016-09/135288.htm
+
+下载mysql源安装包
+wget http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+安装mysql源
+yum localinstall mysql57-community-release-el7-11.noarch.rpm
+检查mysql源是否安装成功
+yum repolist enabled | grep "mysql.*-community.*"
+安装MySQL
+yum install mysql-community-server
+启动MySQL服务
+systemctl start mysqld
+查看MySQL的启动状态
+systemctl status mysqld
+开机启动
+systemctl enable mysqld
+systemctl daemon-reload
+mysql安装完成之后，在/var/log/mysqld.log文件中给root生成了一个默认密码。通过下面的方式找到root默认密码
+grep 'temporary password' /var/log/mysqld.log
+使用默认密码登录
+mysql -uroot -p
+修改root默认密码（必须数字大小写）
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'xxxxxxxxx'; 
+默认只允许root帐户在本地登录，如果要在其它机器上连接mysql，必须修改root允许远程连接，或者添加一个允许远程连接的帐户，为了安全起见，我添加一个新的帐户：
+GRANT ALL PRIVILEGES ON *.* TO 'name'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+配置默认编码为utf8
+修改/etc/my.cnf配置文件，在[mysqld]下添加编码配置，如下所示：
+vi /etc/my.cnf
+[mysqld]
+character_set_server=utf8
+init_connect='SET NAMES utf8'
+重启mysql服务
+systemctl restart mysqld
+
+
+
+
+
+root/jianghe/2053440Zx,
--- app/spa/learn/learnDefaultProprety.js
+++ app/spa/learn/learnDefaultProprety.js
@@ -114,22 +114,103 @@
 //     // return fragment;
 // }
 //
-// // var template = '' +
-// //     '<div>{{name}}</div>';
+
 //
 //
 // // var parentBox = document.querySelector('#parentBox');
+
+
+// function tmpl(str, data) {
+//     var $ = '$' + (+new Date),
+//         fn = function (data) {
+//             var i,
+//                 variable = [$],
+//                 value = [[]];
+//             for (i in data) {
+//                 variable.push(i);
+//                 value.push(data[i]);
+//             }
+//             return (new Function(variable, fn.$)).apply(data, value).join("");
+//         };
+//
+//     //将模板解析成函数
+//     fn.$ = fn.$ || $ + ".push('"
+//         + str.replace(/\\/g, "\\\\")
+//             .replace(/'/g, "\\'")  //防止单括号错误
+//             .replace(/[\r\t\n]/g, " ")
+//             .split("[:").join("\t")
+//             .replace(/((^|:])[^\t]*)'/g, "$1\r")
+//             .replace(/\t=([^\?]*?):]/g, "',$1,'")
+//             .replace(/\t=([^\?]*?)\?(.*?):]/g, "',this.$1||'$2','")   //  [:=data?:]  [:=data?任何内容:]
+//             .split("\t").join("');")
+//             .split(":]").join($ + ".push('")
+//             .split("\r").join("\\'")
+//         + "');return " + $;
+//
+//     //如果未定义data则返回编译好的函数，使用时直接传入数据即可，
+//     //省去每次解析成函数的时间
+//     return data ? fn(data) : fn;
+// };
+//
+// var tpl = '[: for(var k in ary){ var one=ary[k]; :]' +
+//     '<p>[:=one:]</p>' +
+//     '[: } :]';
+// var data = {ary: [123, 'abc']};
+// var div = tmpl(tpl, data);
+// console.log(div); //</p>123</p><p>abc</p>
+
+/**
+ * 监听者
+ * @param he 组件实例山下文
+ * @param matchResult 占位符数组；如：
+ * '<div>{{name}}{{age}}</div>'对应的就是:
+ * [{perch: '{{name}}',result:'name'},{perch: '{{age}}',result:'age'}
+ * @param cb 数据改变时回调方法
+ * @constructor
+ */
+function Watcher(he, matchResult, cb) {
+    this.he = he;
+    this.matchResult = matchResult;
+    this.cb = cb;
+    this.value = this.get();  // 将自己添加到订阅器的操作
+}
+
+Watcher.prototype = {
+    update: function () {
+        this.run();
+    },
+    run: function () {
+        var value = this.he.data[this.matchResult];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.he, value, oldVal);
+        }
+    },
+    get: function () {
+        Dep.target = this;  // 缓存自己
+        /**
+         * 主动执行以下数据拦截(监听器)的get方法让数据拦截器来绑定缓存中的自己(观察者)
+         * 建立监听器和观察者之间的关联：
+         * data <=> 监听器 <=> 观察者 <=> 关系映射器 <=> dom
+         * data的改变会被监听器拦截到,监听器找到相应的观察者
+         */
+        var value = this.he.data[this.matchResult];
+        Dep.target = null;  // 释放自己
+        return value;
+    }
+};
 
 
 /**
  * 映射器用来建立dom(节点)与watcher(观察者)之间的联系(根据dom对象中的占位符来创建观察者)
  * @constructor
  */
-function Mapper(dom, context) {
+function Mapper(dom, he) {
     //需要映射器关联的dom对象
     this.dom = dom;
     //组件上下文对象
-    this.context = context;
+    this.he = he;
     this.init();
 };
 Mapper.prototype = {
@@ -137,6 +218,7 @@ Mapper.prototype = {
     init: function () {
         if (this.dom) {
             this.fragment = this.nodeToFragment(this.dom);
+            this.createRelationship(this.fragment);
         } else {
             console.log('组件未传入dom');
         }
@@ -161,32 +243,125 @@ Mapper.prototype = {
     /**
      * 建立dom和观察的关系
      */
-    createRelationship:function () {
-    }
+    createRelationship: function (fragment) {
+        var that = this,
+            childNodes = fragment.childNodes;
+        [].slice.call(childNodes).forEach(function (node) {
+            if (that.isElementNode(node)) {
+                //Element节点的处理
+
+                //处理类名
+
+                //处理自定义属性
+
+                if (node.childNodes && node.childNodes.length) {
+                    //如果还有子节点递归建立关系
+                    that.createRelationship(node);
+                }
+            } else if (that.isTextNode(node)) {
+                //text节点的处理
+                var reg = /\{\{([A-Za-z0-9_\.]+)\}\}/g,//匹配占位符的正则
+                    matchResult = [],//保存匹配结果
+                    text = node.textContent;//获取元素的纯文本内容
+
+                /**
+                 * 匹配规则{{}}中只允许出现A-Z a-z 0-9 _ .
+                 * 如果出现其他特殊字符会直接把{{}}整体当做字串处理
+                 */
+                while (true) {
+                    var arr = reg.exec(text);
+                    if (!arr) break;
+                    matchResult.push({
+                        perch: arr[0],
+                        result: arr[1]
+                    });
+                }
+
+                if (matchResult.length > 0) {
+                    //只要匹配中matchResult就是一个数组
+                    that.textRule(node, matchResult, text);
+                }
+            }
+        });
+    },
+
+    /**
+     * 文本的更新规则
+     * @param node
+     * @param matchResult
+     * @param ancestorText
+     */
+    textRule(node, matchResult, ancestorText){
+        var that = this,
+            initText = ancestorText;//初始化文本内容
+        matchResult.forEach(function (item) {
+            initText = initText.replace(item.perch, that.he.data[item.result])
+        });
+        this.updateText(node, initText);
+        /**
+         * 创建监听者当数据改变时会执行该回调
+         */
+        new Watcher(this.he, matchResult, function (value) {
+            matchResult.forEach(function (item) {
+                initText = initText.replace(item.perch, that.he.data[item.result])
+            });
+            self.updateText(node, value);
+        });
+    },
+
+    /**
+     * 更新节点的文本内容
+     * @param node
+     * @param value
+     */
+    updateText: function (node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+
+    /**
+     * 判断节点是不是text节点
+     * @param node
+     * @return {boolean}
+     */
+    isTextNode: function (node) {
+        return node.nodeType == 3;
+    },
+
+    /**
+     * 判断节点是不是Element节点
+     * @param node
+     * @return {boolean}
+     */
+    isElementNode: function (node) {
+        return node.nodeType == 1;
+    },
 };
 
+// var template =
+//     '<div id="testdiv">' +
+//     '   <div>' +
+//     '       <div>' +
+//     '           <span>1</span>' +
+//     '       </div>' +
+//     '       <div>' +
+//     '           <span>1</span>' +
+//     '       </div>' +
+//     '   </div>' +
+//     '   <div>' +
+//     '       <div>' +
+//     '           <span>1</span>' +
+//     '       </div>' +
+//     '       <div>' +
+//     '           <span>1</span>' +
+//     '           <div>' +
+//     '               <span>1</span>' +
+//     '           </div>' +
+//     '       </div>' +
+//     '   </div>' +
+//     '</div>';
 var template =
-    '<div id="testdiv">' +
-    '   <div>' +
-    '       <div>' +
-    '           <span>1</span>' +
-    '       </div>' +
-    '       <div>' +
-    '           <span>1</span>' +
-    '       </div>' +
-    '   </div>' +
-    '   <div>' +
-    '       <div>' +
-    '           <span>1</span>' +
-    '       </div>' +
-    '       <div>' +
-    '           <span>1</span>' +
-    '           <div>' +
-    '               <span>1</span>' +
-    '           </div>' +
-    '       </div>' +
-    '   </div>' +
-    '</div>';
+    '<div id="testdiv">sss{{name}}213123{{age}}aaa</div>';
+
 
 function parseDom(html) {
     var objE = document.createElement("div");
@@ -194,7 +369,16 @@ function parseDom(html) {
     return objE;
 }
 
-var mapper = new Mapper(parseDom(template), this);
+var mapper = new Mapper(parseDom(template), {
+    data: {
+        name: 'jianghe',
+        age: 24,
+        worker: {
+            test: 123,
+            test1: 333
+        }
+    }
+});
 
 
 
